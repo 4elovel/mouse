b229d7b4585083c8036f0e8c3842e61c33366096
4elovel <marcynakvova007@gmail.com>
2023-06-14 18:54:59 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MouseMove += Form1_MouseMove;
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point p = e.Location;
+            label1.Text = "Координати миші: х=" + e.X.ToString() + "; y=" + e.Y.ToString();
+        }
+    }
+}
